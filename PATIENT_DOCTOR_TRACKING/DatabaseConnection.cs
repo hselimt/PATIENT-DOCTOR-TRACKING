f7a644006bf5debb574e5cc2a8a8d40cce4c00bb
HST <hselimt@gmail.com>
2025-05-14 13:27:28 +0300

    Add project files.

---
+++ PATIENT_DOCTOR_TRACKING/DatabaseConnection.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace PATIENT_DOCTOR_TRACKING
+{
+    public class DatabaseConnection
+    {
+        // private static string connectionString = @"Data Source=YOUR_SERVER_NAME\SQLEXPRESS;Initial Catalog=PatientDoctorDB;Integrated Security=True;TrustServerCertificate=True";
+
+        public static string GetConnectionString()
+        {
+            try
+            {    
+                 var connectionString = ConfigurationManager.ConnectionStrings["PatientDoctorDB"]?.ConnectionString;
+                 if (string.IsNullOrEmpty(connectionString))
+                 {
+                     return @"Data Source=.\SQLEXPRESS;Initial Catalog=PatientDoctorDB;Integrated Security=True;TrustServerCertificate=True";
+                 }
+                 return connectionString;
+            }
+            
+            catch
+            {
+                return @"Data Source=.\SQLEXPRESS;Initial Catalog=PatientDoctorDB;Integrated Security=True;TrustServerCertificate=True";
+            }
+        }
+
+        public static DataTable ExecuteQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dataTable = new DataTable();
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        public static bool ExecuteNonQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return false;
+                }
+            }
+        }
+
+        public static object ExecuteScalar(string query)
+        {
+            using (SqlConnection connection = new SqlConnection(GetConnectionString()))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    return command.ExecuteScalar();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+![HST Interface](./PDT.png)
+
+# Patient Doctor Tracking System
+
+Windows Forms application for managing patient-doctor relationships and appointments.
+
+## Setup
+
+### Requirements
+- Windows OS
+- Visual Studio 2019+
+- SQL Server Express
+- .NET Framework 4.8
+
+### Running the Application
+1. Run SQL script
+2. Run executable
